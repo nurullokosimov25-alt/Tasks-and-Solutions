d517ebd79428069467236de9eab08ef302a64cd5
nurullokosimov25-alt <nurullokosimov25@gmail.com>
2025-12-25 22:22:37 +0500

    Refactor distances_between_repeats function with comments
    Reformatted the code and added comments explaining the function.

--- ЛР2.2.py
+++ ЛР2.2.py
@@ -1,3 +1,5 @@
+# Найти расстояние между повторениями символа (позиции i и j) Например: s = "abcaab" повтор "a" на позициях 0, 3, 4 → расстояния: 3, 1
+# Использовать словарь-аккумулятор: last_seen[ch] = позиция 
 ﻿def distances_between_repeats(s):
     last_seen = {}      
     distances = []       
@@ -9,3 +11,4 @@
 
     return distances
 print(distances_between_repeats("abcaab"))
+

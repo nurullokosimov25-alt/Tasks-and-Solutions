b49d81ccb6ab4419b3cea48a790d8c2544cbe8ae
nurullokosimov25-alt <nurullokosimov25@gmail.com>
2025-12-25 22:29:49 +0500

    Update palindrome check for new model name

--- ЛР5.2.py
+++ ЛР5.2.py
@@ -2,6 +2,7 @@
 def is_palindrome(model_name):
     clean_name = "".join(model_name.split()).lower()
     return clean_name == clean_name[::-1]
-model = "Àçèçà"
-print(f"ßâëÿåòñÿ ëè '{model}' ïàëèíäðîìîì?:", is_palindrome(model))
+model = "Aziza"
+print(f"Является ли '{model}' палиндромом?:", is_palindrome(model))
+
 
